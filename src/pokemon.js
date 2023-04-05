3b2fcd4121603f9831e2eab3ca42d689b98fd1bf
Akxn <shen.yen112@gmail.com>
2023-04-04 23:07:06 -0700

    added pokemon box

--- src/pokemon.js
+++ src/pokemon.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-function Pokemon({ pokemon }) {
-  const getThreeDigitId = (id) => {
-    if (id < 10) return `00${id}`
-    if (id < 100) return `0${id}`
-    return id
+function PokemonModal({ showModal, setShowModal, pokemon }) {
+  if (!showModal) {
+    return null;
   }
 
+  const imageUrl = `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${('00' + pokemon.id).slice(-3)}.png`;
+
   return (
-    <>
-      <img src={`https://github.com/fanzeyi/pokemon.json/raw/master/images/${getThreeDigitId(pokemon.id)}.png`} alt="pokemonImg" />
-    </>
-  )
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="close-modal" onClick={() => setShowModal(false)}>
+          Close
+        </button>
+        <h2>#{pokemon.id} {pokemon.name.english}</h2>
+        <img className="pokemon-image" src={imageUrl} alt={pokemon.name.english} />
+        <div className="pokemon-type">
+          {pokemon.type.join(' ')}
+        </div>
+        <table>
+          <tbody>
+            {Object.entries(pokemon.base).map(([stat, value]) => (
+              <tr key={stat}>
+                <td className="stat">{stat}</td>
+                <td>{value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Pokemon
+export default PokemonModal;
